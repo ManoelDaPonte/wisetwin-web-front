dd82d626ad58bf4a812f3ea42f3c77903256a374
Manoel Da Ponte <131278442+ManoelDaPonte@users.noreply.github.com>
2024-05-13 18:13:26 +0200

    contact form update

--- components/contactPage/BeContactedForm.jsx
+++ components/contactPage/BeContactedForm.jsx
@@ -1,20 +1,43 @@
 import styles from "@/styles/contactPage/beContactedForm.module.css";
 import { useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 export default function BeContactedForm() {
-	// Use the useState hook to create a state variable and a function to update it
+	const [notification, setNotification] = useState({
+		show: false,
+		message: "",
+		success: false,
+	});
 	const searchParams = useSearchParams();
 	const interest = searchParams.get("interest");
 
 	async function handleSubmit(event) {
 		event.preventDefault();
-
 		const formData = new FormData(event.target);
 
 		const response = await fetch("/api/sendMailContact", {
 			method: "POST",
 			body: formData,
-		}).then((res) => res.json());
+		});
+
+		const data = await response.json(); // Parse JSON seulement après avoir vérifié le statut HTTP
+		console.log(data);
+
+		if (response.ok) {
+			// Si le statut HTTP indique un succès
+			setNotification({
+				show: true,
+				message: "Email sent successfully!",
+				success: true,
+			});
+		} else {
+			// Si le statut HTTP indique un échec
+			setNotification({
+				show: true,
+				message: data.message || "Failed to send email.",
+				success: false,
+			});
+		}
 	}
 
 	return (
@@ -24,6 +47,17 @@ export default function BeContactedForm() {
 				Fill in the form below and we will contact you as soon as
 				possible with the right person to help you.
 			</div>
+			{notification.show && (
+				<div
+					className={
+						notification.success
+							? styles.successBanner
+							: styles.errorBanner
+					}
+				>
+					{notification.message}
+				</div>
+			)}
 			<form className={styles.form} onSubmit={handleSubmit}>
 				<input
 					type="text"
