5f0f10741410a36544343e701e1dff4bc3339805
wise-twin <wisetwin.contact@gmail.com>
2024-06-11 16:32:29 +0200

    page solutions
    page solutions

--- app/page.jsx
+++ app/page.jsx
@@ -7,20 +7,20 @@ const HomePage = () => {
 	const cards = [
 		{
 			title: "Our Solutions",
-			text: "We brought sci-fi ideas to reality. Discover our digital twin solutions.",
-			image: "image/png/turbine-exploded.png",
+			text: "We brought sci-fi to reality. Discover our digital twin solutions, and our innovative approach to enhance your daily tasks.",
+			image: "image/png/digital-twin-recompressor-wise-twin.png",
 			href: "/solutions",
 		},
 		{
 			title: "Uses Cases",
-			text: "How our digital twin solution can help you.",
+			text: "Discover how our digital twins can help you, with use cases that drive real-world impact",
 			image: "image/png/our-solutions.jpg",
 			href: "/use-cases",
 		},
 		{
 			title: "Industries",
-			text: "Discover our solutions for the industry.",
-			image: "image/png/ai-modeling.png",
+			text: "Boost your industrial performance by increasing efficiency and productivity. Let our AI-assistant provide you data-driven insights and help reduce costs.",
+			image: "image/png/turbine-exploded.png",
 			href: "/industries",
 		},
 	];
